87a91810783a78aa7d5e0facff68472045a620c3
JP Rosa <33404287+joaop-rosa@users.noreply.github.com>
2022-07-06 20:14:27 -0300

    Add files via upload

---
+++ PROG2_TrabalhoFinalG2/bibliotecas/missoes.h
@@ -0,0 +1,11 @@
+//
+// Created by jprag on 26/06/2022.
+//
+
+#ifndef PROG2_TRABALHOFINALG2_MISSOES_H
+#define PROG2_TRABALHOFINALG2_MISSOES_H
+
+MISSAO selecionarMissao();
+PERSONAGEM realizarMissao(MISSAO missao, PERSONAGEM personagem);
+int tamanhoVetor(int p[]);
+#endif //PROG2_TRABALHOFINALG2_MISSOES_H
